bda9653700645c59d4f23cdca99e336cbe2e169c
Jason Gehl <jason.r.gehl@gmail.com>
2018-06-10 19:48:49 -0400

    Updating packages to avoid vulnerabilities

--- build_scripts/generateMockData.js
+++ build_scripts/generateMockData.js
@@ -5,6 +5,10 @@ import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
 
+jsf.extend('faker', function() {
+  return require('faker');
+});
+
 const json = JSON.stringify(jsf(schema));
 
 fs.writeFile('./src/api/db.json', json, function(err){
