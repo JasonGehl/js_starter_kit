2ed8bd107d3d48d07240d4d78ec31bb40ab3cc7d
Jason Gehl <jason.r.gehl@gmail.com>
2018-06-10 15:19:14 -0400

    Add ESLint support, including watch script

--- build_scripts/srcServer.js
+++ build_scripts/srcServer.js
@@ -3,6 +3,7 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+/* eslint-disable no-console */
 
 //set port number and start Express
 const PORT_NUMBER = 3000;
