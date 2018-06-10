46b6064ebb5974a8a39c88e4c5791216a82f024b
Jason Gehl <jason.r.gehl@gmail.com>
2018-06-10 14:59:24 -0400

    Add and set up Webpack compiler for bundling code, including css

--- build_scripts/srcServer.js
+++ build_scripts/srcServer.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 //set port number and start Express
 const PORT_NUMBER = 3000;
 const app = express();
+
+//set up the webpack compiler
+const compiler = webpack(config);
+
+//set the app to use webpack
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 //handle routes with function
 app.get('/', function(request, response){
